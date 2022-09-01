9c25e8c7e769408748886d0078725d51e1d0280e
Thiago Lins <falecom@thiagolins.dev.br>
2022-09-01 00:11:21 -0300

    [Update] Theme And Add Material UI  Theme Provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,18 +3,24 @@ import { ThemeProvider } from 'styled-components'
 import { AuthProvider } from '@contexts/AuthContext'
 import Routes from '@app/routes'
 import GlobalStyles from '@styles/global'
-import defaultTheme from '@styles/themes'
+import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles'
+import defaultTheme, { MUIDefaultTheme } from '@styles/themes/default'
+import CssBaseline from '@mui/material/CssBaseline'
+
 import './App.css'
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <React.StrictMode>
-        <GlobalStyles />
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
-      </React.StrictMode>
-    </ThemeProvider>
+    <MUIThemeProvider theme={MUIDefaultTheme}>
+      <ThemeProvider theme={defaultTheme}>
+        <React.StrictMode>
+          <CssBaseline />
+          <GlobalStyles />
+          <AuthProvider>
+            <Routes />
+          </AuthProvider>
+        </React.StrictMode>
+      </ThemeProvider>
+    </MUIThemeProvider>
   )
 }
 
