28c3ab44ae0db203f4eda322398a865489fab1ae
Thiago Lins <falecom@thiagolins.dev.br>
2022-09-01 01:33:20 -0300

    [Add] Toast Container

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,14 +5,15 @@ import Routes from '@app/routes'
 import GlobalStyles from '@styles/global'
 import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles'
 import defaultTheme, { MUIDefaultTheme } from '@styles/themes/default'
+import ToastContainer from '@components/ToastContainer'
 import CssBaseline from '@mui/material/CssBaseline'
-
 function App() {
   return (
     <MUIThemeProvider theme={MUIDefaultTheme}>
       <ThemeProvider theme={defaultTheme}>
         <React.StrictMode>
           <CssBaseline />
+          <ToastContainer />
           <GlobalStyles />
           <AuthProvider>
             <Routes />
